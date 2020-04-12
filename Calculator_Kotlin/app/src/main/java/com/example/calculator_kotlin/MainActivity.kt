6628b54527743d3ee2eb8c7f245c06ab77adcefd
alibek <zhumash6@gmail.com>
2020-04-12 23:18:06 +0600

    11

--- Calculator_Kotlin/app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
+++ Calculator_Kotlin/app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.calculator_kotlin
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -62,7 +64,9 @@ class MainActivity : AppCompatActivity() {
         val s = tv.text.toString()
         val infoTextView = findViewById<TextView>(R.id.tvExpression)
         val text = infoTextView.text.toString()
-        if (text == "0") infoTextView.text = s else infoTextView.append(s)
+        if (text.equals("0")) {
+            infoTextView.text = s
+        } else infoTextView.append(s)
         val tvO = findViewById<TextView>(R.id.tvOut)
         tvO.text = ""
     }
@@ -119,6 +123,394 @@ class MainActivity : AppCompatActivity() {
                 operation = "+"
             }
         }
+    }
+
+    fun pressMinus(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        val s = tv.text.toString()
+        if (s != "") {
+            if (!sign) {
+                tvO.text = ""
+                sign = true
+                num1 = tv.text.toString()
+                tv.text = ""
+                operation = "-"
+            } else {
+                operation = "-"
+            }
+        }
+    }
+
+    fun pressPercent(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        val s = tv.text.toString()
+        if (s != "") {
+            if (!sign) {
+                tvO.text = ""
+                sign = true
+                if (s != "") {
+                    num1 = tv.text.toString()
+                    tv.text = ""
+                    operation = "%"
+                }
+            } else {
+                operation = "%"
+            }
+        }
+    }
+
+    fun pressMulti(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        val s = tv.text.toString()
+        if (s != "") {
+            if (!sign) {
+                tvO.text = ""
+                sign = true
+                if (s != "") {
+                    num1 = tv.text.toString()
+                    tv.text = ""
+                    operation = "*"
+                }
+            } else {
+                operation = "*"
+            }
+        }
+    }
+
+    fun pressDiv(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        val s = tv.text.toString()
+        if (s != "") {
+            if (!sign) {
+                tvO.text = ""
+                sign = true
+                if (s != "") {
+                    num1 = tv.text.toString()
+                    tv.text = ""
+                    operation = "/"
+                }
+            } else {
+                operation = "/"
+            }
+        }
+    }
+
+
+    fun pressSqrt() {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val text = tv.text.toString()
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        if (text != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = sqrt(number1)
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun pressPi(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val pi = Math.PI
+        tv.text = pi.toString()
+    }
+
+
+    fun pressLog(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = log10(number1)
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun pressLogN(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = ln(number1)
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    private fun factorial(n: Int): Int {
+        return if (n == 0) 1 else n * factorial(n - 1)
+    }
+
+    fun pressFactorial(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toInt().toDouble()
+            output = factorial(number1.toInt()).toDouble()
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun pressDouble(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = number1 * number1
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun pressPower(view: View) {
+        if (!sign) {
+            val tv = findViewById<TextView>(R.id.tvExpression)
+            val tvO = findViewById<TextView>(R.id.tvOut)
+            tvO.text = ""
+            val s = tv.text.toString()
+            sign = true
+            if (s != "") {
+                num1 = tv.text.toString()
+                tv.text = ""
+                operation = "^"
+            }
+        } else {
+            operation = "^"
+        }
+    }
+
+    fun degToRad(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            if (!rad) {
+                output = Math.toRadians(number1)
+                out = output.toString()
+                tv.text = out
+                rad = true
+            }
+        }
+    }
+
+    fun radianToD(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            if (rad) {
+                output = Math.toDegrees(number1)
+                out = output.toString()
+                tv.text = out
+                rad = false
+            }
+        }
+    }
+
+    fun sin(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            if (!rad) {
+                output = sin(Math.toRadians(number1))
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            } else {
+                output = sin(number1)
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            }
+        }
+    }
+
+    fun cos(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            if (!rad) {
+                output = cos(Math.toRadians(number1))
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            } else {
+                output = cos(number1)
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            }
+        }
+    }
+
+    fun tan(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            if (!rad) {
+                output = tan(Math.toRadians(number1))
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            } else {
+                output = tan(number1)
+                tv.text = ""
+                out = output.toString()
+                tvO.append(out)
+            }
+        }
+    }
+
+    fun exp(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = exp(number1)
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun inverse(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        tvO.text = ""
+        val s = tv.text.toString()
+        if (s != "") {
+            num1 = tv.text.toString()
+            number1 = num1!!.toDouble()
+            output = 1 / number1
+            tv.text = ""
+            out = output.toString()
+            tvO.append(out)
+        }
+    }
+
+    fun pressEquals(view: View) {
+        val tv = findViewById<TextView>(R.id.tvExpression)
+        val tvO = findViewById<TextView>(R.id.tvOut)
+        val s = tv.text.toString()
+        tvO.setTextColor(Color.rgb(255, 255, 255))
+        if (s != "") {
+            if (operation == "+") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                output = number1 + number2
+                out = output.toString()
+                tvO.text = out
+                operation = ""
+                sign = false
+            }
+            if (operation == "-") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                output = number1 - number2
+                out = output.toString()
+                tvO.text = out
+                operation = ""
+                sign = false
+            }
+            if (operation == "*") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                output = number1 * number2
+                out = output.toString()
+                tvO.text = out
+                operation = ""
+                sign = false
+            }
+            if (operation == "/") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                if (number1 == 0.0 || number2 == 0.0) {
+                    tvO.setTextColor(Color.rgb(249, 174, 171))
+                    tvO.text = "На 0 делить нельзя"
+                } else {
+                    output = number1 / number2
+                    out = output.toString()
+                    tvO.text = out
+                    operation = ""
+                }
+                operation = ""
+                sign = false
+            }
+            if (operation == "%") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                val percent = number2 / 100
+                output = number1 * percent
+                out = output.toString()
+                tvO.text = out
+                operation = ""
+                sign = false
+            }
+            if (operation == "^") {
+                num2 = tv.text.toString()
+                number1 = num1!!.toDouble()
+                number2 = num2!!.toDouble()
+                output = Math.pow(number1, number2)
+                out = output.toString()
+                tvO.text = out
+                operation = ""
+                sign = false
+            }
+        }
+        tv.text = ""
+        rad = false
     }
 
 
--- Retrofit_App/app/src/main/java/com/example/retrofit_app/MainActivity.kt
+++ Retrofit_App/app/src/main/java/com/example/retrofit_app/MainActivity.kt
@@ -2,11 +2,60 @@ package com.example.retrofit_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainFragment: MainFragment
+    lateinit var favoriteFragment: FavoriteFragment
+    lateinit var profileFragment: ProfileFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigation_bar)
+
+        mainFragment = MainFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_layout, mainFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.main_menu -> {
+                    mainFragment = MainFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_layout, mainFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.favorites_menu -> {
+                    favoriteFragment = FavoriteFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_layout, favoriteFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.profile_menu -> {
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_layout, profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+
+            }
+            true
+        }
     }
 }
